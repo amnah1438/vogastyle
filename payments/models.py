cb76a5fa456796558f9b4f69938077e2721b703b
amnah1438 <__@domain.com>
2025-11-14 01:25:08 +0300

    تعديل اللغه في المودل في البيييي

--- payments/models.py
+++ payments/models.py
@@ -2,9 +2,20 @@ from django.db import models
 from orders.models import Order
 
 class Payment(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-    method = models.CharField(max_length=50)  # مثل: Card, ApplePay...
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        verbose_name="الطلب"
+    )
+    amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name="المبلغ"
+    )
+    method = models.CharField(
+        max_length=50,
+        verbose_name="طريقة الدفع"  # مثل ApplePay, Card, Mada
+    )
     status = models.CharField(
         max_length=20,
         choices=[
@@ -12,9 +23,13 @@ class Payment(models.Model):
             ("success", "ناجح"),
             ("failed", "فشل"),
         ],
-        default="pending"
+        default="pending",
+        verbose_name="حالة الدفع"
     )
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="تاريخ العملية"
+    )
 
     class Meta:
         verbose_name = "دفعة"
