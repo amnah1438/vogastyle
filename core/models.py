81f5b7ec43f377ef279bc7f75fcbc4fec865f972
amnah1438 <__@domain.com>
2025-11-14 01:21:39 +0300

    تعديل اللفه في المدل في الكور

--- core/models.py
+++ core/models.py
@@ -1,10 +1,26 @@
 from django.db import models
 
 class SiteSetting(models.Model):
-    site_name = models.CharField(max_length=100, default="VogaStyle")
-    logo = models.ImageField(upload_to='logo/', blank=True, null=True)
-    footer_text = models.TextField(blank=True, null=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    site_name = models.CharField(
+        max_length=100,
+        default="VogaStyle",
+        verbose_name="اسم الموقع"
+    )
+    logo = models.ImageField(
+        upload_to='logo/',
+        blank=True,
+        null=True,
+        verbose_name="شعار الموقع"
+    )
+    footer_text = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name="نص الفوتر"
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name="تاريخ التحديث"
+    )
 
     class Meta:
         verbose_name = "إعداد الموقع"
