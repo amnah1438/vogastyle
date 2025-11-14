15372e0d7de422369c2c227858f99f4b4e63cf23
amnah1438 <__@domain.com>
2025-11-14 12:11:24 +0300

    تعديل اليوآر ال في الاوكنت

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,22 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = "accounts"
 
 urlpatterns = [
+    # تسجيل حساب جديد
     path("register/", views.register_view, name="register"),
+
+    # تسجيل الدخول
     path("login/", views.login_view, name="login"),
+
+    # تسجيل الخروج
+    path("logout/", auth_views.LogoutView.as_view(next_page="/"), name="logout"),
+
+    # الملف الشخصي
+    path("profile/", views.profile_view, name="profile"),
+
+    # تعديل الملف الشخصي
+    path("profile/update/", views.update_profile_view, name="update_profile"),
 ]
