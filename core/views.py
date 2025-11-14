7186730dd047c9eed5f0cfc580acad865ecfbe19
amnah1438 <__@domain.com>
2025-11-14 23:27:55 +0300

    تعديل

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from catalog.models import Product   # <-- مهم جداً
 
 
@@ -21,3 +21,15 @@ def home(request):
     }
 
     return render(request, "core-templates/home.html", context)
+
+
+# ============================
+# تغيير الدولة (Country Switch)
+# ============================
+def set_country(request):
+    if request.method == "POST":
+        country = request.POST.get("country")
+        request.session["country"] = country   # نحفظها في الجلسة Session
+
+    # نرجّع المستخدم لنفس الصفحة التي كان عليها
+    return redirect(request.META.get("HTTP_REFERER", "/"))
