505e871b684ef06754a62e4ce90c12404cb8203b
amnah1438 <__@domain.com>
2025-11-14 23:26:56 +0300

    تعديل الكور

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,11 @@ from . import views
 app_name = 'core'
 
 urlpatterns = [
-    path('', views.home, name='home'),       # الصفحة الرئيسية
-    path('splash/', views.splash, name='splash'),   # شاشة البداية
+    path('', views.home, name='home'),                     # الصفحة الرئيسية
+    path('splash/', views.splash, name='splash'),          # شاشة البداية
+
+    # ============================
+    #  مسار تغيير الدولة (جديد)
+    # ============================
+    path('set-country/', views.set_country, name='set_country'),
 ]
