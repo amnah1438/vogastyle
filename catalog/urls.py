710ed763984dfc232ffb3bdd61c979de18ee35ef
amnah1438 <__@domain.com>
2025-11-14 14:15:10 +0300

    تعديل اليو آر لي في كتالوج

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,18 +4,12 @@ from . import views
 app_name = 'catalog'
 
 urlpatterns = [
-    # ============================
-    # قائمة المنتجات
-    # ============================
     path('', views.product_list, name='product_list'),
-
-    # ============================
-    # المنتجات حسب التصنيف
-    # ============================
     path('category/<slug:category_slug>/', views.products_by_category, name='products_by_category'),
+    path('<slug:product_slug>/', views.product_detail, name='product_detail'),
 
     # ============================
-    # صفحة تفاصيل المنتج
+    # الصفحة الجديدة (العروض)
     # ============================
-    path('<slug:product_slug>/', views.product_detail, name='product_detail'),
+    path('offers/', views.offers, name='offers'),
 ]
