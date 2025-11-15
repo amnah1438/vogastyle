fc8959d0390bc3d2840ad3b739b4e9692814c5c9
amnah1438 <__@domain.com>
2025-11-15 15:05:19 +0300

    تعديلات جديده على الكل

--- static/js/header.js
+++ static/js/header.js
@@ -1,23 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    const trigger = document.getElementById("countryTrigger");
-    const menu = document.getElementById("countryMenu");
+    const trigger = document.getElementById("vgCountryTrigger");
+    const menu = document.getElementById("vgCountryMenu");
 
     if (trigger && menu) {
-
         trigger.addEventListener("click", function (e) {
             e.stopPropagation();
             menu.classList.toggle("show");
         });
 
-        document.addEventListener("click", function (e) {
-            if (!trigger.contains(e.target)) {
-                menu.classList.remove("show");
-            }
-        });
-
-        menu.addEventListener("click", function (e) {
-            e.stopPropagation();
+        document.addEventListener("click", function () {
+            menu.classList.remove("show");
         });
     }
+
 });
